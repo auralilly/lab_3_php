e7313f19cfa6f48e718a0859397a9312499c752a
Caleb <caleb.hunt789@gmail.com>
2026-02-02 02:41:42 +0000

    config set up

---
+++ lab3/Config.php
@@ -0,0 +1,9 @@
+<?php
+// Configuration settings
+define('TO_EMAIL', 'Caleb.hunt789@gmail.com');          
+define('EMAIL_SUBJECT', 'New Contact Form Message');
+define('FROM_NAME', 'Contact Form');
+define('FROM_EMAIL_DOMAIN', $_SERVER['HTTP_HOST'] ?? 'example.com');
+
+
+?>
