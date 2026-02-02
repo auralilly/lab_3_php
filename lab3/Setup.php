5bca88a6a2788c8f20ae7096d013e5b7db45a026
Caleb <caleb.hunt789@gmail.com>
2026-02-02 02:43:13 +0000

    stil a lot more to do but got to update

--- lab3/Setup.php
+++ lab3/Setup.php
@@ -11,3 +11,30 @@ $form_data = [
     'email'      => '',
     'message'    => ''
 ];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get and trim inputs
+    $first_name = trim($_POST['first_name'] ?? '');
+    $last_name  = trim($_POST['last_name']  ?? '');
+    $email      = trim($_POST['email']      ?? '');
+    $message    = trim($_POST['message']    ?? '');
+
+    // Store for repopulation
+    $form_data = [
+        'first_name' => $first_name,
+        'last_name'  => $last_name,
+        'email'      => $email,
+        'message'    => $message
+    ];
+
+    // ---------------- SERVER-SIDE VALIDATION ----------------
+    if (empty($first_name)) {
+        $errors[] = "First name is required.";
+    } elseif (strlen($first_name) < 2 || strlen($first_name) > 50) {
+        $errors[] = "First name must be between 2 and 50 characters.";
+    }
+
+    if (empty($last_name)) {
+        $errors[] = "Last name is required.";
+    } elseif (strlen($last_name) < 2 || strlen($last_name) > 50) {
+        $errors[] = "Last name must be between 2 and 50 characters.";
+    }
