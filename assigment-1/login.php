7173fb587fba99566d88067eed619944d9db84c1
Caleb <caleb.hunt789@gmail.com>
2026-04-10 21:54:21 +0000

    almost done the html

--- assigment-1/login.php
+++ assigment-1/login.php
@@ -19,3 +19,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login - Team Tracker</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
