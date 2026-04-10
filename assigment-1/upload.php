64348b15d1479f55c88a26e97cd66a8264687570
Caleb <caleb.hunt789@gmail.com>
2026-04-10 22:05:20 +0000

    pictures

--- assigment-1/upload.php
+++ assigment-1/upload.php
@@ -12,3 +12,20 @@ if (!$member) {
     header("Location: index.php");
     exit;
 }
+$error = '';
+$success = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['photo'])) {
+    $file = $_FILES['photo'];
+    
+    // limiting the pictures
+    $allowed_types = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
+    $max_size = 5 * 1024 * 1024; 
+
+    if ($file['error'] !== 0) {
+        $error = "Error uploading file.";
+    } elseif (!in_array($file['type'], $allowed_types)) {
+        $error = "Only JPG, PNG, GIF, and WebP images are allowed.";
+    } elseif ($file['size'] > $max_size) {
+        $error = "File is too large. Maximum size is 5MB.";
+    } else {
