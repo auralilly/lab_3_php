c7cdbc3aa5f5223acb51947fdd13ea6c9b4ca2dd
Caleb <caleb.hunt789@gmail.com>
2026-04-10 20:31:06 +0000

    filtering out invalids

--- assigment-1/register.php
+++ assigment-1/register.php
@@ -5,3 +5,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = trim($_POST['username'] ?? '');
     $email    = trim($_POST['email'] ?? '');
     $password = $_POST['password'] ?? '';
+
+    $errors = [];
+
+    if (strlen($username) < 3) $errors[] = "Username must be at least 3 characters.";
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) $errors[] = "Valid email is required.";
+    if (strlen($password) < 6) $errors[] = "Password must be at least 6 characters.";
