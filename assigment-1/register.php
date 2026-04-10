31e706071ce18eb323cf93b4c3df3098f4940a27
Caleb <caleb.hunt789@gmail.com>
2026-04-10 20:30:29 +0000

    setting up the register

---
+++ assigment-1/register.php
@@ -0,0 +1,7 @@
+<?php
+require 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username'] ?? '');
+    $email    = trim($_POST['email'] ?? '');
+    $password = $_POST['password'] ?? '';
